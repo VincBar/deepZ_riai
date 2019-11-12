a0f6ef91843e79edfc34b50d066c980e17e8c7df
VincBar <51025193+VincBar@users.noreply.github.com>
2019-11-12 16:35:00 +0100

    got everything updated

--- code/networks.py
+++ code/networks.py
@@ -75,7 +75,9 @@ class Conv(nn.Module):
 
 
 class NNFullyConnectedZ(nn.Module):
-
+    """
+    Copy of fullyConnected Module for the image. This module builds up the corresponding network for the Zonotope.
+    """
     def __init__(self, device, input_size, fc_layers):
         super(NNFullyConnectedZ, self).__init__()
 
@@ -84,6 +86,7 @@ class NNFullyConnectedZ(nn.Module):
         for i, fc_size in enumerate(fc_layers):
             layers += [LinearZ(prev_fc_size, fc_size)]
             if i + 1 < len(fc_layers):
+                #why fc_size
                 layers += [ReLUZLinear(fc_size)]
             prev_fc_size = fc_size
         self.layers = nn.Sequential(*layers)
@@ -136,12 +139,10 @@ class LinearZ(nn.Linear):
 
     def forward(self, x):
         # input (N, K, input_size)
-        N, K, input_size = x.shape
-        x = x.view(N * K, input_size)
-        out = super(LinearZ, self).forward(x).view(N, K, -1)
-        out[:, 0, :] += self.bias
-
-        return
+        out = x.matmul(self.weight.t())
+        out[0, :] += self.bias
+        # fix return out ?
+        return out
 
 
 class ConvZ(nn.Conv2d):
@@ -176,11 +177,12 @@ class ReLUZ(nn.Module):
 
     @staticmethod
     def lower_bound(x):
-        return torch.abs(x[:, 0, :, :, :]) - torch.sum(torch.abs(x[:, 1:, :, :, :]), dim=2)
+        return x[0, :] - torch.sum(torch.abs(x[1:, :]), dim=0)
 
+    #bugfix, we dont want the absolut value of x[0]
     @staticmethod
     def upper_bound(x):
-        return torch.abs(x[:, 0, :,  :, :]) + torch.sum(torch.abs(x[:, 1:, :, :, :]), dim=2)
+        return x[0, :] + torch.sum(torch.abs(x[1:, :]), dim=0)
 
     def weights_init(self):
         # TODO: Initialize weights here
@@ -221,28 +223,32 @@ class ReLUZLinear(ReLUZ):
     def __init__(self, fc_size):
         super(ReLUZLinear, self).__init__()
 
-        self.lambdas = nn.Parameter(torch.ones([1, 1, fc_size]))
+        self.lambdas = nn.Parameter(torch.eye(fc_size))
         self.lambdas.requires_grad_()
 
         self.relu = nn.ReLU()
 
     def forward(self, x):
         # input is (N, K, fc_size)
-        N, K, fc_size = x.shape
+        #N, K, fc_size = x.shape
 
         # TODO: I don't know if this is computed in parallel, if written like this
         l, u = self.lower_bound(x), self.upper_bound(x)
 
-        # TODO: check if broadcasting of lambdas works as expected
-        l_0_u = nn.functional.relu(u) * nn.functional.relu(-l)
-        out = nn.functional.relu(l) * x + l_0_u * self.lambdas * x
-        out[:, :, 0] -= self.lambdas[:, :, 0] * x / 2
+        # # TODO: check if broadcasting of lambdas works as expected
+        l_0_u = torch.le(torch.mul(l,u),0)
+        u_0 = torch.ge(u,0)
 
-        fc_size = x[1]
-        K = x.shape[2]
-        out = nn.functional.pad(input=out, pad=(0, 0, 0, fc_size, 0, 0), mode='constant', value=0)
+        out = x - torch.mul(x,l_0_u) + x.matmul(self.lambdas)/2
+        out[0,:]= - self.lambdas.matmul(l)/2
 
-        out[:, K:, :] = - self.lambdas * l / 2 * l_0_u
+        # out[:, :, 0] -= self.lambdas[:, :, 0] * x / 2
+        #
+        # fc_size = x[1]
+        # K = x.shape[2]
+        # out = nn.functional.pad(input=out, pad=(0, 0, 0, fc_size, 0, 0), mode='constant', value=0)
+        #
+        # out[:, K:, :] = - self.lambdas * l / 2 * l_0_u
 
         return out
 
