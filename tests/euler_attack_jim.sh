d7fddc53666f9f411bd11a8b533f291a7d9d478e
Unknown <rrrr@posteo.ch>
2019-12-01 13:36:17 +0100

    update bsub

--- tests/euler_attack_jim.sh
+++ tests/euler_attack_jim.sh
@@ -4,3 +4,5 @@ git pull
 git checkout redo_bounds
 
 conda activate riai-project
+
+bsub -n 10 -W 1:00 -J "attack" -R "rusage[mem=3072]" -e attack_err_log '--pairwise 0 --nr_eps 5 --n_jobs_per_digit 5 --maxsec 120 --check_smaller 1 --n_digits 2 --n_jobs 2'  
